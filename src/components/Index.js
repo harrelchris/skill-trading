4f38c4aa6418f754da408a3c7e4c7fcdd1663f6a
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-03-06 14:20:42 -0600

    display evepraisal prices in a table

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Evepraisal from "../api/evepraisal"
 
 function Index() {
-  return <h1>Index</h1>;
+  const evepraisal = new Evepraisal();
+  const [prices, setPrices] = useState({});
+
+  function renderItem(name) {
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>{prices[name].buy}</td>
+        <td>{prices[name].sell}</td>
+      </tr>
+    );
+  }
+
+  useEffect(() => {
+    evepraisal.getPrices().then((prices) => setPrices(prices));
+  }, []);
+
+  return (
+    <>
+      <h1>Index</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Buy</th>
+            <th>Sell</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(prices).map((name) => renderItem(name))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default Index;
