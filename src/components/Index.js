7c4593981a843c3fae72223ba07ffb9dbb3750c6
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-03-07 21:49:30 -0600

    refactor

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,15 +1,25 @@
-import React, { useEffect, useState } from "react";
-import ISKTable from "./ISKTable";
+import React from "react";
+import Header from "./Header";
+import Inputs from "./Inputs";
+import Outputs from "./Outputs";
+
+function isAuthenticated() {
+  const requiredKeys = ["accessToken", "refreshToken", "expiresAt", "name", "id"];
+
+  for (let key of requiredKeys) {
+    if (localStorage.getItem(key) === null) {
+      return false;
+    }
+  }
+  return true;
+}
 
 function Index() {
-  const [biologyLevel, setBiologyLevel] = useState(5);
-  const [isOmega, setIsOmega] = useState(true);
-
   return (
     <>
-      <h1>Index</h1>
-      <button onClick={e => setIsOmega(!isOmega)}>{isOmega ? "Omega" : "Alpha"}</button>
-      <ISKTable />
+      <Header isAuthenticated={isAuthenticated} />
+      <Inputs />
+      <Outputs />
     </>
   );
 }
