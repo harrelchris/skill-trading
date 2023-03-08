ee3dba3e21c6553f268065e454c9c99128e71177
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-03-07 21:49:15 -0600

    create user menu - avatar and menu

---
+++ src/components/Header/UserMenu.js
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Avatar from "@mui/material/Avatar";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import {Link} from "react-router-dom";
+
+function UserMenu() {
+  const name = localStorage.getItem("name");
+  const id = localStorage.getItem("id");
+  const portraitURL = `https://images.evetech.net/characters/${id}/portrait?size=64`
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
+  return (
+    <>
+      <Avatar
+        alt={name}
+        variant="rounded"
+        src={portraitURL}
+        sx={{ width: 48, height: 48 }}
+
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      />
+      <ArrowDropDownIcon
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      />
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Link to="/auth/logout">Logout</Link>
+        </MenuItem>
+      </Menu>
+
+    </>
+  );
+}
+
+export default UserMenu;
