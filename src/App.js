62b3dacbdd0af49aa8b7edb4b7eb4813379b73f9
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-03-07 21:47:47 -0600

    restructure as spa

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
+import CssBaseline from '@mui/material/CssBaseline';
 
 import Authorize from "./components/auth/Authorize";
 import Callback from "./components/auth/Callback";
-import Dashboard from "./components/Dashboard";
 import Index from "./components/Index";
 import Logout from "./components/auth/Logout";
-import Navbar from "./components/Navbar";
-import NotFound from "./components/NotFound";
-import Protected from "./components/Protected";
 
 function useQuery() {
   const { search } = useLocation();
   return React.useMemo(() => new URLSearchParams(search), [search]);
-}
-
-function isAuthenticated() {
-  const requiredKeys = ["accessToken", "refreshToken", "expiresAt", "name", "id"];
-
-  for (let key of requiredKeys) {
-    if (localStorage.getItem(key) === null) {
-      return false;
-    }
-  }
-  return true;
 }
 
 function App() {
@@ -31,30 +17,12 @@ function App() {
 
   return (
     <>
-      <Navbar isAuthenticated={isAuthenticated}/>
+      <CssBaseline />
       <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="*" element={<NotFound />} />
-        <Route
-          path="/auth/logout"
-          element={
-            <Protected isAuthenticated={isAuthenticated}>
-              <Logout />
-            </Protected>
-          } />
-        <Route
-          path="/dashboard"
-          element={
-            <Protected isAuthenticated={isAuthenticated}>
-              <Dashboard />
-            </Protected>
-          } />
+        <Route path="*" element={<Index />} />
         <Route path="/auth/authorize" element={<Authorize />} />
-        <Route
-          path="/auth/callback"
-          element={
-            <Callback code={query.get("code")} state={query.get("state")} />
-        } />
+        <Route path="/auth/callback" element={<Callback code={query.get("code")} state={query.get("state")} />} />
+        <Route path="/auth/logout" element={<Logout />} />
       </Routes>
     </>
   );
