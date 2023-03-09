549a8c48b89241576a3e56a059e65488d01fa3f8
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-03-08 21:52:43 -0600

    get prices

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./Header";
-import Inputs from "./Inputs";
-import Outputs from "./Outputs";
+import Injectors from "./Injectors";
+import Accelerators from "./Accelerators";
+import Evepraisal from "../api/evepraisal";
 
 function isAuthenticated() {
   const requiredKeys = ["accessToken", "refreshToken", "expiresAt", "name", "id"];
@@ -15,11 +16,39 @@ function isAuthenticated() {
 }
 
 function Index() {
+  const ep = new Evepraisal();
+  const [prices, setPrices] = useState({
+    "Large Skill Injector": {
+      buy: 0,
+      sell: 0,
+    },
+    "Small Skill Injector": {
+      buy: 0,
+      sell: 0,
+    },
+    "Daily Alpha Injector": {
+      buy: 0,
+      sell: 0,
+    },
+    "Expert Cerebral Accelerator": {
+      buy: 0,
+      sell: 0,
+    },
+    "Master-at-Arms Cerebral Accelerator": {
+      buy: 0,
+      sell: 0,
+    },
+  });
+
+  useEffect(() => {
+    ep.getPrices().then(priceData => setPrices(priceData));
+  }, []);
+
   return (
     <>
       <Header isAuthenticated={isAuthenticated} />
-      <Inputs />
-      <Outputs />
+      <Injectors prices={prices} />
+      <Accelerators prices={prices} />
     </>
   );
 }
